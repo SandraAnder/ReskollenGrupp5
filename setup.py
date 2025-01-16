8bacbe8109e57804ae2e6d8ef1107befba6a2502
Sandra Andersson <sandy.andersson.6@gmail.com>
2025-01-16 21:57:17 +0100

    Setup of project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+print(find_packages(exclude=["test*", "explorations"]))
+
+setup(
+    name="travel_planner",
+    version="0.0.1",
+    description="""This ackage is userd for travel planning in public transport.
+    It has backend, frontend and utils.""",
+    author="Sandra Andersson",
+    author_email="dvasdvjnaaadvjn@gmail.com",
+    install_packages=["streamlit", "pandas", "requests",
+                      "folium", "pprint", "python-dotenv"],
+    packages=find_packages(exclude=["test*", "explorations"]),
+    entry_points={"console_scripts": [
+        "dashboard = utils.run_dashboard:run_dashboard"]},
+)
