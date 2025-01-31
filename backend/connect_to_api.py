10b9cc1857fdb8927a72fc82e7d740fe1a573ace
Sandra Andersson <sandy.andersson.6@gmail.com>
2025-01-31 20:22:19 +0100

    FRONTEND-Add-stylesheet: Sorting DRY code in trips.py and fix for arrivals not showing

--- backend/connect_to_api.py
+++ backend/connect_to_api.py
@@ -1,21 +1,21 @@
-from dotenv import load_dotenv
 import os
-import requests
-from pprint import pprint
-import re
-import pandas as pd
 from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class ResRobot:
-
     API_KEY = os.getenv("API_KEY")
 
     def trips(self, origin_id=740000001, destination_id=740098001):
         """origing_id and destination_id can be found from Stop lookup API"""
-        url = f"https://api.resrobot.se/v2.1/trip?format=json&originId={origin_id}&destId={destination_id}&passlist=true&showPassingPoints=true&accessId={self.API_KEY}"
+        url = f"""https://api.resrobot.se/v2.1/trip?format=json&originId={origin_id}
+        &destId={destination_id}&passlist=true&
+        showPassingPoints=true&accessId={self.API_KEY}"""
 
         try:
             response = requests.get(url)
@@ -26,7 +26,8 @@ class ResRobot:
             print(f"Network or HTTP error: {err}")
 
     def access_id_from_location(self, location):
-        url = f"https://api.resrobot.se/v2.1/location.name?input={location}&format=json&accessId={self.API_KEY}"
+        url = f"""https://api.resrobot.se/v2.1/location.name?input={location}
+        &format=json&accessId={self.API_KEY}"""
         response = requests.get(url)
         result = response.json()
 
@@ -43,13 +44,16 @@ class ResRobot:
 
     # Function to extract the stopname
     def get_stop_name(self, location):
-        url_stopName = f"https://api.resrobot.se/v2.1/location.name?input={location}&format=json&accessId={self.API_KEY}"
+        url_stopName = f"""https://api.resrobot.se/v2.1/location.name?input={location}
+        &format=json&accessId={self.API_KEY}"""
 
         result_name = requests.get(url_stopName)
         return result_name.json().get("stopLocationOrCoordLocation")
 
     def get_departures(self, location_ids, max_results=8):
-        url_departures = f"https://api.resrobot.se/v2.1/departureBoard?id={location_ids}&format=json&maxJourneys={max_results}&accessId={self.API_KEY}"
+        url_departures = f"""https://api.resrobot.se/v2.1/departureBoard?
+        id={location_ids}&format=json&maxJourneys={max_results}&
+        accessId={self.API_KEY}"""
 
         response = requests.get(url_departures)
         data = response.json()
@@ -57,8 +61,9 @@ class ResRobot:
 
         return departures[:max_results]
 
-    def get_arrivials(self, location_ids, max_results=8):
-        url_arrivals = f"https://api.resrobot.se/v2.1/arrivalBoard?id={location_ids}&format=json&maxJourneys={max_results}&accessId={self.API_KEY}"
+    def get_arrivals(self, location_ids, max_results=8):
+        url_arrivals = f"""https://api.resrobot.se/v2.1/arrivalBoard?id={location_ids}
+        &format=json&maxJourneys={max_results}&accessId={self.API_KEY}"""
         response = requests.get(url_arrivals)
 
         data_arrivals = response.json()
@@ -69,7 +74,8 @@ class ResRobot:
 
     # Filter one hour ahead v
     def get_departures_around_one_hour_ahead(self, stop_id):
-        url = f"https://api.resrobot.se/v2.1/departureBoard?id={stop_id}&format=json&accessId={self.API_KEY}"
+        url = f"""https://api.resrobot.se/v2.1/departureBoard?id={stop_id}
+        &format=json&accessId={self.API_KEY}"""
 
         response = requests.get(url)
         if response.status_code != 200:
@@ -77,7 +83,9 @@ class ResRobot:
             return None
 
         departures = pd.DataFrame(response.json().get("Departure", []))
-        departures["datetime"] = pd.to_datetime(departures["date"] + " " + departures["time"], errors='coerce')
+        departures["datetime"] = pd.to_datetime(
+            departures["date"] + " " + departures["time"], errors="coerce"
+        )
         departures = departures.dropna(subset=["datetime"])
 
         if departures.empty:
@@ -85,8 +93,14 @@ class ResRobot:
             return None
 
         one_hour_ahead = datetime.now() + timedelta(hours=1)
-        time_range = [one_hour_ahead - timedelta(minutes=15), one_hour_ahead + timedelta(minutes=15)]
-        departures_filtered = departures[(departures["datetime"] >= time_range[0]) & (departures["datetime"] <= time_range[1])]
+        time_range = [
+            one_hour_ahead - timedelta(minutes=15),
+            one_hour_ahead + timedelta(minutes=15),
+        ]
+        departures_filtered = departures[
+            (departures["datetime"] >= time_range[0])
+            & (departures["datetime"] <= time_range[1])
+        ]
 
         if departures_filtered.empty:
             print(f"No departures within ±15 minutes for stop_id {stop_id}.")
@@ -94,10 +108,8 @@ class ResRobot:
 
         departures_filtered["stop_id"] = stop_id
         return departures_filtered[["name", "stop", "direction", "datetime", "stop_id"]]
-    # Filter one hour ahead ^
-    
 
-    
+    # Filter one hour ahead ^
 
 
 if __name__ == "__main__":
