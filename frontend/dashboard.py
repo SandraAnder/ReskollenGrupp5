97522b160cd3f03846feedcdb48a26e580bd5752
Sandra Andersson <sandy.andersson.6@gmail.com>
2025-02-02 21:33:55 +0100

    FRONTEND-Styling-and-background-image: Add banner, logo and some styling for h1 and p

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,11 +1,11 @@
+from pathlib import Path
+
 import streamlit as st
 
 from backend.connect_to_api import ResRobot
 from backend.trips import StopPlanner
 from frontend.plot_maps import TripMap
 from utils.constants import StationIds
-from pathlib import Path
-
 
 st.set_page_config(
     page_title="Reskollen",  # Titel på browserfliken
@@ -24,25 +24,61 @@ tripPlanner = StopPlanner("")
 
 def render_table_without_index(df):
     # Konverterar DataFrame till HTML utan indexkolumnen
-    table_html = df.to_html(index=False, header=True, classes='styled-table')
+    table_html = df.to_html(index=False, header=True, classes="styled-table")
     st.markdown(table_html, unsafe_allow_html=True)
 
 
 def main():
+    st.markdown(
+        """
+    <div class="logo-container">
+        <img src="""
+        + (
+            "https://static.vecteezy.com/system/resources/previews/027/529/362/"
+            "non_2x/tour-bus-and-map-with-traffic-navigation-route-location-marker-"
+            "scheme-flat-illustration-for-passenger-traffic-service-vector.jpg"
+        )
+        + """
+        alt="Logotyp" class="logo">
+    </div>
+    """,
+        unsafe_allow_html=True,
+    )
 
-    st.markdown("""
+    # Custom HTML/CSS for the banner
+    banner_img = (
+        """
+    <div class="banner">
+        <img src="""
+        + (
+            "https://static.vecteezy.com/ti/gratis-vektor/p1/14041676-metro-"
+            "underjordisk-och-tunnelbana-rader-transport-karta-vector.jpg"
+        )
+        + """
+        alt="Banner Image">
+    </div>"""
+    )
+    # Visar bannern med spec höjd
+    st.components.v1.html(banner_img, height=350)
+
+    st.markdown(
+        """
     <div class="header-container">
         <h1>Reseplanerare</h1>
-        <p>Den här dashboarden syftar till att både utforska data för olika platser, <br>men ska även fungera som en reseplanerare där du får välja och planera din resa.</p>
+        <p>
+            Den här dashboarden syftar till att både utforska data för olika platser,
+            <br>men ska även fungera som en reseplanerare där du får välja och
+            planera din resa.
+        </p>
     </div>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
     # Skapar en meny i sidofältet till vänster
     st.sidebar.title("Meny")
     st.sidebar.markdown("### Välj sida")
-    page = st.sidebar.radio(
-        label="",
-        options=["Sök resa", "Karta"])
+    page = st.sidebar.radio(label="", options=["Sök resa", "Karta"])
 
     if page == "Karta":
         trip_map.display_map()
@@ -61,7 +97,8 @@ def main():
                 departures_df = tripPlanner.get_timetable_dep(station)
                 if not departures_df.empty:
                     departures_df = departures_df.reset_index(
-                        drop=True)  # Tar bort index i DataFramen
+                        drop=True
+                    )  # Tar bort index i DataFramen
                     render_table_without_index(departures_df)
                 else:
                     st.write("Inga avgångar hittades.")
@@ -81,12 +118,10 @@ def main():
 
 
 def read_css():
-    css_path = Path(__file__).parent / '../style.css'
+    css_path = Path(__file__).parent / "../style.css"
 
     with open(css_path) as css:
-        st.markdown(
-            f'<style>{css.read()}</style>', unsafe_allow_html=True
-        )
+        st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
