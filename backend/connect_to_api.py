06684d990c2b7860cf8b038119db036f9a6b6c38
Sandra Andersson <sandy.andersson.6@gmail.com>
2025-02-05 09:58:10 +0100

    STREAMLIT-Deploy-app: Added streamlit in api request

--- backend/connect_to_api.py
+++ backend/connect_to_api.py
@@ -3,13 +3,14 @@ from datetime import datetime
 
 import pandas as pd
 import requests
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class ResRobot:
-    API_KEY = os.getenv("API_KEY")
+    API_KEY = st.secrets.get("API_KEY", os.getenv("API_KEY"))
 
     def trips(self, origin_id, destination_id):
         """origing_id and destination_id can be found from Stop lookup API"""
